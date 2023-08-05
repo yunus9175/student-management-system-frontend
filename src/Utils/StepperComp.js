d09de67043e0a6dbe6051c76232d415f18f0542c
alim <mohdalim8180036208@gmail.com>
2023-08-05 17:59:01 +0530

    update carousel

--- src/Utils/StepperComp.js
+++ src/Utils/StepperComp.js
@@ -1,10 +1,7 @@
-import React, { memo } from "react";
-import { useTheme } from "@mui/material/styles";
-import Box from "@mui/material/Box";
-import SwipeableViews from "react-swipeable-views";
-import { autoPlay } from "react-swipeable-views-utils";
-
-const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
+import React from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
+import { Box, useMediaQuery } from "@mui/material";
 
 const images = [
   "https://images.unsplash.com/photo-1492538368677-f6e0afe31dcc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
@@ -17,42 +14,44 @@ const images = [
 ];
 
 const SwipeableTextMobileStepper = () => {
-  const theme = useTheme();
-  const [activeStep, setActiveStep] = React.useState(0);
-
-
-
-  const handleStepChange = (step) => {
-    setActiveStep(step);
-  };
-
+  const matches = useMediaQuery("(min-width:600px)");
   return (
-    <Box sx={{ width: "100%", height: "100%", flexGrow: 1 }}>
-      <AutoPlaySwipeableViews
-        axis={theme.direction === "rtl" ? "x-reverse" : "x"}
-        index={activeStep}
-        onChangeIndex={handleStepChange}
-        enableMouseEvents
-      >
-        {images.map((image, index) => (
-          <Box key={index} sx={{ width: "100%", height: "100%" }}>
-            {Math.abs(activeStep - index) <= 2 ? (
-              <Box
-                component="img"
-                sx={{
-                  height: "89vh",
-                  display: "block",
-                  overflow: "hidden",
-                  width: "100%",
-                }}
-                src={image}
-              />
-            ) : null}
-          </Box>
-        ))}
-      </AutoPlaySwipeableViews>
-    </Box>
+    <Carousel
+      showArrows={false}
+      showStatus={false}
+      showThumbs={false}
+      infiniteLoop={true}
+      autoPlay={true}
+      interval={3000}
+      stopOnHover={false}
+      transitionTime={500}
+      emulateTouch={true}
+      swipeable={true}
+      style={{ height: "100%" }}
+    >
+      {images.map((img, index) => (
+        <Box
+          key={index}
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            height: "100%",
+          }}
+        >
+          <img
+            src={img}
+            alt="Image"
+            style={{
+              objectFit: "cover", // Fit both width and height within the container
+              width: "100%",
+              height: !matches ? "100%" : "90vh",
+            }}
+          />
+        </Box>
+      ))}
+    </Carousel>
   );
 };
 
-export default memo(SwipeableTextMobileStepper);
+export default SwipeableTextMobileStepper;
